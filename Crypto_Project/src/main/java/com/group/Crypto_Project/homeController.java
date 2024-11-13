79e6f99aa4c6256d85b7e9135f9475412cc4ca39
sivasaikoya <sivasaikoya@localhost>
2024-11-14 02:00:43 +0530

    test

--- Crypto_Project/src/main/java/com/group/Crypto_Project/homeController.java
+++ Crypto_Project/src/main/java/com/group/Crypto_Project/homeController.java
@@ -3,6 +3,7 @@ package com.group.Crypto_Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class homeController {
@@ -10,6 +11,13 @@ public class homeController {
 	@RequestMapping("/")
 	public String home() {
 		return "index";
+	}
+	
+
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "hello this is test";
 	}
 	//for test
 
